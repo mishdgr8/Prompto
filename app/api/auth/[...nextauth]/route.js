8dbe46336866cb7253c63a73a6c49e0f8b5299e5
mishdgr8 <Mishaelmordi@gmail.com>
2023-06-28 19:44:37 +0100

    4TH COMMIT

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,8 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { connectToDatabase } from "@utils/database";
+
 import User from "@models/users";
+import { connectToDatabase } from "@utils/database";
 
 const handler = NextAuth({
   providers: [
@@ -12,31 +13,31 @@ const handler = NextAuth({
   ],
   callbacks: {
     async session({ session }) {
-      const sessionUser = await User.findOne({
-        email: session.user.email,
-      });
+      // store the user id from MongoDB to session
+      const sessionUser = await User.findOne({ email: session.user.email });
       session.user.id = sessionUser._id.toString();
+
       return session;
     },
-    async signIn({ profile }) {
+    async signIn({ account, profile, user, credentials }) {
       try {
         await connectToDatabase();
 
-        //check if user exists
+        // check if user already exists
         const userExists = await User.findOne({ email: profile.email });
 
-        //if user does not exist, create user
+        // if not, create a new document and save user in MongoDB
         if (!userExists) {
           await User.create({
             email: profile.email,
             username: profile.name.replace(" ", "").toLowerCase(),
-            image: profile.image,
+            image: profile.picture,
           });
-
-          return true;
         }
+
+        return true;
       } catch (error) {
-        console.log(error);
+        console.log("Error checking if user exists: ", error.message);
         return false;
       }
     },
